8a3b8f9b16470593e4e73c6d3819e256940c003d
Steve White <stephenkrohnwhite@gmail.com>
2018-03-20 19:22:50 -0500

    add fizzbuzz final

---
+++ Fizzbuzz.js
@@ -0,0 +1,17 @@
+"use strict";
+function logFizzBuzz() {
+    for(let i = 0; i <= 100; i++) {
+	    if(i % 3 === 0 && i % 5 === 0) {
+		    console.log("fizzbuzz");
+	    } else if(i % 5 === 0) {
+		    console.log("buzz");
+    	} else if(i % 3 === 0) {
+		    console.log("fizz");
+	    } else {
+    		console.log(i);
+	    }
+	}
+}
+
+logFizzBuzz();
+		
